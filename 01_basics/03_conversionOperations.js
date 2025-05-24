4a0721cd68340cfd6326e211cc3f6b4cb3af6fd3
Amaresh Maharana <amareshcoder@gmail.com>
2025-05-24 16:56:54 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+let footballScore = "8ab"
+
+console.log(typeof footballScore);
+
+let scoreStringToNumber = Number(footballScore)
+// after converting of string to number
+console.log(scoreStringToNumber);
+console.log(typeof scoreStringToNumber);
+
+
+let randomNo = 94
+
+// converting number to string
+let randomNoToString = String(randomNo)
+console.log("After number to string conversion: ", randomNoToString);
+console.log(typeof randomNoToString);
+
