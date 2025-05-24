f113e7d38b41bff881c6911ac21f78f578d68fc6
Amaresh Maharana <amareshcoder@gmail.com>
2025-05-24 16:00:56 +0000

    datatypes defined

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js coe as newer version
+
+// alert(3 + 3) -> we are using node, it can only run on browser
+
+// datatypes:
+
+/*
+    PRIMITIVE DATA TYPES:
+
+ -> Number
+ -> String
+ -> Boolean
+ -> Undefined
+ -> Null - type is object (imp.)
+ -> Symbol
+ -> Bigint
+
+
+    NON-PRIMITIVE DATA TYPES:
+
+ -> Object
+ -> Array
+ -> Function
+*/
