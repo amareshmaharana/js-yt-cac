ed92ad43fb0c20b5f9884e95922d7ba4b4dc3026
Amaresh Maharana <amareshcoder@gmail.com>
2025-05-25 16:03:17 +0000

    comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// Basic conversions
+
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+// like this conversions often times feel confusing
+console.log("2" < 1);
+console.log("1" == 0);
+console.log("\n");
+
+console.log(null > 0); // false
+console.log(null <= 0); // true - because here null equal with 0
+console.log(null == 0); // false
+
+
+// === -> this comparison define or check - values with data types
+console.log("2" === 2); // it return false
+
+
+// javascript automatically convert to number when comparing with numbers
