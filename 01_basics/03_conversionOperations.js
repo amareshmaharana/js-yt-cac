306eeec5a9934e7fb7f7224de6762cb81f20a653
Amaresh Maharana <amareshcoder@gmail.com>
2025-05-25 15:52:05 +0000

    conversion operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,17 +1,27 @@
 let footballScore = "8ab"
 
-console.log(typeof footballScore);
+// console.log(typeof footballScore);
 
 let scoreStringToNumber = Number(footballScore)
 // after converting of string to number
-console.log(scoreStringToNumber);
-console.log(typeof scoreStringToNumber);
+// console.log(scoreStringToNumber);
+// console.log(typeof scoreStringToNumber);
 
 
 let randomNo = 94
 
 // converting number to string
 let randomNoToString = String(randomNo)
-console.log("After number to string conversion: ", randomNoToString);
-console.log(typeof randomNoToString);
+// console.log("After number to string conversion: ", randomNoToString);
+// console.log(typeof randomNoToString);
 
+
+/* ************** Operations **************** */
+
+// don't write like this code mean this additions with string
+console.table([
+    "1" + 2,
+    1 + "2",
+    "1" + 2 + 2,
+    1 + 2 + "2"
+])
