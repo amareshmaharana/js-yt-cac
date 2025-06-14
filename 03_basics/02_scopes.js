531837f81c981359eb42ac5722c168edf8d9bf21
Amaresh Maharana <amareshcoder@gmail.com>
2025-06-14 16:50:02 +0000

    more about of scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -15,4 +15,51 @@ if(true) {
     console.log('Block scope:', a, b, c); // printing block scope variables
 }
 
-console.log(a, b, c); // printing global scope variables
+// console.log(a, b, c); // printing global scope variables
+
+
+
+function one() {
+    const username = 'John';
+
+    function two() {
+        const website = 'twitch';
+        console.log(username); // can access username from one
+    }
+
+    console.log(website); // cannot access website from two
+    two(); // calling two function
+}
+
+// one(); // calling one function
+
+
+
+if (true) {
+    const handlename = 'John';
+
+    if (handlename === "John") {
+        const website = 'twitch';
+        console.log(handlename + website); // can access both handlename and website
+    }
+    // console.log(website); // cannot access website outside the inner if block
+}
+
+// console.log(handlename)
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// here in this case we can call before initialization
+function addOne(num) {
+    return num + 1
+}
+
+addOne(6)
+
+
+console.log(addTwo(6)) // here gives a error because addTwo is not defined yet mean it is called before initialization
+const addTwo = function(num) {
+    return num + 2
+}
